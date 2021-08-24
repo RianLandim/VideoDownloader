2e4e063c502b88fcff5f27c8f09712f257857c1d
Juan Dantas <rianlandim22@gmail.com>
2021-08-24 18:22:23 -0300

    Added a Dir input

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
 import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import Progress
+from pytube import YouTube
+from pytube.cli import on_progress
+
 
 sg.theme('Reddit')
 
+
+def Download(link, dir):
+    youtube = YouTube(link, on_progress_callback=on_progress)
+    video = youtube.streams.get_highest_resolution()
+    video.download(f'/{dir or "Downloads"}')
+
+
 layout = [
-    [sg.Text('Coloque aqui o link:'), sg.InputText()],
+    [sg.Text('Coloque aqui o link:'), sg.InputText(key='link',)],
+    [sg.Text('Escolha a pasta: '), sg.InputText(key='dir')],
     [sg.Button('Baixar')]
 ]
 
@@ -13,5 +25,7 @@ while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, 'cancel'):
         break
+    if event == 'Baixar':
+        Download(values['link'], values['dir'])
 
 window.close()
